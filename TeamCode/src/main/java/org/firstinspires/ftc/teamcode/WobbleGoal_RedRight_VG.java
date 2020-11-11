0a893fedcaafae468194e17002e8d2af839daed9
Team10477 <rapidrobotsnovi@gmail.com>
2020-11-11 16:10:44 -0500

    Clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal_RedRight_VG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal_RedRight_VG.java
@@ -52,32 +52,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
-/**
- * This file illustrates the concept of driving a path based on time.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backwards for 1 Second
- *   - Stop and close the claw.
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @Autonomous(name="Moochi_lah", group="Wobble Goal")
 public class WobbleGoal_RedRight_VG extends LinearOpMode {
-
-    /* Declare OpMode members. */
-// The objects below are classes that help us connect to the robot hardware
 
     private DcMotor leftFrontDrive = null;
     private DcMotor rightFrontDrive = null;
